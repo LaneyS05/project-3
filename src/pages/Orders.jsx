2e15e7d55043584654d15e3fd03fade6431ed680
Warlie Yap <137013560+warlie3d@users.noreply.github.com>
2024-03-19 18:11:25 -0400

    added Orders page with sorting and table

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids'
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy'
+import { Header } from '../components'
+import { resize } from '@syncfusion/ej2/maps'
+import { pdfExport } from '@syncfusion/ej2/pivotview'
+
 
 const Orders = () => {
     return (
-        <div>Orders</div>
+        <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+            <Header category="Page" title="Orders" />
+            <GridComponent
+                id='gridcomp'
+                dataSource={ordersData}
+                //to limit items on orders page
+                allowPaging
+                //Allows user to sort orders
+                allowSorting
+            >
+                <ColumnsDirective>
+                    {ordersGrid.map((item, index) =>
+                    (<ColumnDirective
+                        key={index} {...item}
+                    />))}
+                </ColumnsDirective>
+                {/* to show pages under orders */}
+                <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]} />
+            </GridComponent>
+        </div>
     )
 }
 
